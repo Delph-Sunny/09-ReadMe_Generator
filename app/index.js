c2ed2e3361b91e3f18ac4b04f3fc144abf57c735
Delph <delph_teb@hotmail.com>
2021-01-11 22:12:00 -0500

    complete writefile function and util req

--- app/index.js
+++ app/index.js
@@ -2,16 +2,15 @@
 const inquirer = require("inquirer");
 // creating variable for file system dependancy
 const fs = require("fs");
-// creating variable for
-//const util = require("util");
+// creating variable for util in order to use promisify
+const util = require("util");
 // creating variable for generateMarkdown.js, which contains the function to build a ReadMe
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create a function to write README file
+// Function to write README file
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-// TODO: Create an array of questions for user input
+// Array of questions for user inputs
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -80,26 +79,18 @@ const promptUser = () => {
     ])
 }
 
-/* To replace with the utils generateMarkdown.js
-const generateMD = (answers) => {
-
-}
-
-*/
-
-// TODO: Create a function to initialize app
+// Function to initialize the app
 const init = async () => {
     try {
         const answers = await promptUser();
         console.log(answers);       // FOR TESTING
-        const readme = generateMD(answers);
+        const readme = generateMarkdown(answers);
         console.log(readme);       // FOR TESTING 
-        await fs.writeFileAsync("README.md", readme)
-        console.log("README.md written successfully")
+        await fs.writeFileAsync("README.md", readme);
+        console.log("README.md written successfully");
     } catch (error) {
         console.log(error);
     }
-
 }
 
 // Function call to initialize app
